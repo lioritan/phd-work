def658ccdcaf3f85adf896fc5e68c48f68ff61ed
lioritan <lioritan@users.noreply.github.com>
2021-01-17 17:18:02 +0200

    Sanity test for teacher-student

--- curriculum_code/curriculum/teacher.py
+++ curriculum_code/curriculum/teacher.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 
 from curriculum.history import History
+from curriculum.student import Student
 
 
 class Teacher(ABC):
@@ -11,7 +12,7 @@ class Teacher(ABC):
         self.history = History(history_parameters)
         self.seed = seed
 
-    def train_single_episode(self, student: BaseAlgorithm, max_episode_length: int):
+    def train_single_episode(self, student: Student, max_episode_length: int):
         training_task = self.generate_task()
         trajectory, rewards = self.train_student_on_task(student, training_task, max_episode_length)
         self.history.update(training_task, trajectory, rewards)
@@ -19,15 +20,11 @@ class Teacher(ABC):
 
     def train_student_on_task(self, student, training_task, max_episode_length):
         action_limit = max_episode_length
-        student.set_env(training_task)
-        if isinstance(student, OffPolicyAlgorithm):
-            student.learn(total_timesteps=action_limit, terminate_on_done=True)
-        elif isinstance(student, OnPolicyAlgorithm):
-            student.learn(action_limit, callback=StopTrainingOnMaxEpisodes(1))
-            student.__train()
+        student.train_episode(training_task, action_limit)
+
         # TODO: run student for action_limit steps. Can this be done with student.learn?
-        print(student)
-        print(student.replay_buffer)
+        #print(student)
+        #print(student.replay_buffer)
         # TODO: return the trajectory and rewards
         return [], []
 
--- curriculum_code/run_experiment.py
+++ curriculum_code/run_experiment.py
@@ -1,2 +1,32 @@
 # TODO: parse teacher params, parse env params, run & evaluate
 # consider splitting some of the code here
+import gym
+from stable_baselines3.common.utils import constant_fn
+import stable_baselines3.dqn as dqn
+
+from curriculum.students.dqn import DQN
+from curriculum.teacher import Teacher
+
+
+class SanTeacher(Teacher):
+
+    def update_teacher_policy(self):
+        pass
+
+    def generate_task(self):
+        return gym.make('CartPole-v1')
+
+
+def check_sanity():
+    #student = PPO(policy='MlpPolicy', env="CartPole-v1", batch_size=1)
+    env = gym.make("CartPole-v1")
+    policy = dqn.MlpPolicy(observation_space=env.observation_space, action_space=env.action_space,
+                           lr_schedule=constant_fn(0.1))
+    policy = policy.to("cuda")
+    student = DQN(policy, env, learning_rate=0.1, learning_starts=10, gradient_steps=1, tau=0.8)
+
+    teacher = SanTeacher(None, None)
+    for i in range(101):
+        teacher.train_single_episode(student, 200)
+
+check_sanity()
