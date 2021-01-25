b02b2ceb089d887d7e56e51e7c862ac191ff8064
lioritan <lioritan@users.noreply.github.com>
2021-01-25 12:50:09 +0200

    Add simple gridworld, lunarlander

---
+++ curriculum_code/environment/simple_envs/parametric_gridworld.py
@@ -0,0 +1,28 @@
+import gym_minigrid
+
+from typing import Any, Dict
+
+import gym
+
+from environment.environment_parameter import ContinuousParameter, CategoricalParameter
+from environment.environment_wrapper import EnvironmentWrapper
+
+
+class GridworldsWrapper(EnvironmentWrapper):
+    def __init__(self):
+        super().__init__()
+        self.name = "Gridworlds"
+        self.parameters = {
+            "room_size": CategoricalParameter([3, 4, 6, 14]),
+            "has_key": CategoricalParameter([False, True]),
+        }
+
+    def create_env(self, parameter_values: Dict[str, Any]):
+        room_size = parameter_values["room_size"] + 2  # buffer walls don't count
+        if parameter_values["has_key"]:
+            base_env = gym.make(f"MiniGrid-DoorKey-{room_size}x{room_size}-v0")
+        else:
+            base_env = gym.make(f"MiniGrid-Empty-{room_size}x{room_size}-v0")
+
+        base_env.reset()
+        return base_env
---
+++ curriculum_code/environment/simple_envs/parametric_lunarlander.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict
+
+import gym
+import gym.envs.box2d.lunar_lander as lander
+
+from environment.environment_parameter import ContinuousParameter
+from environment.environment_wrapper import EnvironmentWrapper
+
+
+class LunarLanderWrapper(EnvironmentWrapper):
+    def __init__(self):
+        super().__init__()
+        self.name = "LunarLander-v2"
+        self.parameters = {
+            "leg_height": ContinuousParameter(5, 50),
+            "leg_width": ContinuousParameter(2, 10),
+            "main_engine_power": ContinuousParameter(5.0, 100.0),
+            "side_engine_power": ContinuousParameter(0.0, 10.0),
+        }
+
+    def create_env(self, parameter_values: Dict[str, Any]):
+        base_env = gym.make("LunarLander-v2")
+        lander.MAIN_ENGINE_POWER = parameter_values["main_engine_power"]
+        lander.SIDE_ENGINE_POWER = parameter_values["side_engine_power"]
+        lander.LEG_H = parameter_values["leg_height"]
+        lander.LEG_W = parameter_values["leg_width"]
+        lander.INITIAL_RANDOM = 1500
+
+        base_env.reset()
+        return base_env
