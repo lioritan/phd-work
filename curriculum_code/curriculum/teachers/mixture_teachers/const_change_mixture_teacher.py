a336dcdc867f5760866adc0b40bd3ad138d22ef4
lioritan <lioritan@users.noreply.github.com>
2021-01-26 11:36:51 +0200

    Simplest mixture teacher

---
+++ curriculum_code/curriculum/teachers/mixture_teachers/const_change_mixture_teacher.py
@@ -0,0 +1,33 @@
+
+
+from stable_baselines3.common.type_aliases import GymEnv
+import numpy as np
+from curriculum.teacher import Teacher
+from typing import Tuple, Dict, Any
+
+
+class ConstMixtureTeacher(Teacher):
+    def __init__(self, teacher_parameters, environment_parameters):
+        super().__init__(teacher_parameters, environment_parameters)
+        self.step_size = teacher_parameters["step_size"]
+        self.teachers = teacher_parameters["teachers"]
+        self.teacher_weights = np.ones(len(self.teachers))/len(self.teachers)
+        self.chosen_teacher = 0
+
+    def generate_task(self) -> Tuple[GymEnv, Dict[str, Any]]:
+        choice_idx = np.random.choice(np.arange(stop=len(self.teachers)), size=1, p=self.teacher_weights)
+        self.chosen_teacher = choice_idx
+        return self.teachers[choice_idx].generate_task()
+
+    def update_teacher_policy(self):
+        self.teachers[self.chosen_teacher].history.history.append(self.history[-1])
+        self.teachers[self.chosen_teacher].update_teacher_policy()
+
+        teacher_history = self.teachers[self.chosen_teacher].history
+        if len(teacher_history) == 1:
+            return
+        else:
+            last_reward = teacher_history[-2][1]
+            new_reward = teacher_history[-1][1]
+            self.teacher_weights = self.teacher_weights - (self.step_size/len(self.teachers))
+            self.teacher_weights[self.chosen_teacher] += self.step_size * np.sign(new_reward - last_reward)
