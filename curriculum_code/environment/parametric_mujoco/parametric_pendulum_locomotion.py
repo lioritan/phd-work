516c8d8128f16ce9e4020b004310fdfb349f0984
lioritan <lioritan@users.noreply.github.com>
2021-06-03 14:53:42 +0300

    Getting ready for runs with fixed sched

--- curriculum_code/environment/parametric_mujoco/parametric_pendulum_locomotion.py
+++ curriculum_code/environment/parametric_mujoco/parametric_pendulum_locomotion.py
@@ -19,13 +19,13 @@ class PendRewardModel(StateActionReward):
 
     def state_reward(self, state, next_state=None):
         angle_cost = -0.5 * (self.cos_angle - state[..., 0]) ** 2 - 0.5 * (self.sin_angle - state[..., 1]) ** 2
-        return 100*angle_cost - 0.1 * state[..., 2] ** 2
+        return angle_cost - 0.1 * state[..., 2] ** 2
 
 
 class AngledPendulumEnv(PendulumEnv):
     def __init__(self, angle=0.0):
         super(AngledPendulumEnv, self).__init__()
-        self.angle = angle_normalize(angle)
+        self.angle = angle #angle_normalize(angle)
         self.m = 0.1
 
     def step(self, u):
@@ -38,8 +38,8 @@ class AngledPendulumEnv(PendulumEnv):
 
         u = np.clip(u, -self.max_torque, self.max_torque)[0]
         self.last_u = u  # for rendering
-        angle_cost = (self.angle - angle_normalize(th)) ** 2
-        costs = 100*angle_cost + .1 * thdot ** 2 + .001 * (u ** 2)
+        angle_cost = angle_normalize(self.angle - th) ** 2
+        costs = angle_cost + .1 * thdot ** 2 + .001 * (u ** 2)
 
         newthdot = thdot + (-3 * g / (2 * l) * np.sin(th + np.pi) + 3. / (m * l ** 2) * u) * dt
         newth = th + newthdot * dt
