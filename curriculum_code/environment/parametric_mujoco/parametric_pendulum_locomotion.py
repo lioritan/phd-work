4b61bc5d605d9285c8f022f1966d136706840083
lioritan <lioritan@users.noreply.github.com>
2021-06-10 11:26:07 +0300

    Prep for exps

--- curriculum_code/environment/parametric_mujoco/parametric_pendulum_locomotion.py
+++ curriculum_code/environment/parametric_mujoco/parametric_pendulum_locomotion.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 
 import torch
 import numpy as np
+from gym import spaces
 from gym.envs.classic_control import PendulumEnv
 from gym.envs.classic_control.pendulum import angle_normalize
 
@@ -23,10 +24,22 @@ class PendRewardModel(StateActionReward):
 
 
 class AngledPendulumEnv(PendulumEnv):
-    def __init__(self, angle=0.0):
+    def __init__(self, angle=0.0, randomize_start=False, goal_in_state=False):
         super(AngledPendulumEnv, self).__init__()
-        self.angle = angle #angle_normalize(angle)
+        self.angle = angle
         self.m = 0.1
+        self.goal_in_state = goal_in_state
+        self.radomize_start = randomize_start
+
+        if self.goal_in_state:
+            high = np.array([1., 1., self.max_speed, 1., 1.], dtype=np.float32)
+        else:
+            high = np.array([1., 1., self.max_speed], dtype=np.float32)
+        self.observation_space = spaces.Box(
+            low=-high,
+            high=high,
+            dtype=np.float32
+        )
 
     def step(self, u):
         th, thdot = self.state  # th := theta
@@ -39,7 +52,7 @@ class AngledPendulumEnv(PendulumEnv):
         u = np.clip(u, -self.max_torque, self.max_torque)[0]
         self.last_u = u  # for rendering
         angle_cost = angle_normalize(self.angle - th) ** 2
-        costs = angle_cost + .1 * thdot ** 2 + .001 * (u ** 2)
+        costs = angle_cost + .1 * thdot ** 2 #+ .001 * (u ** 2)
 
         newthdot = thdot + (-3 * g / (2 * l) * np.sin(th + np.pi) + 3. / (m * l ** 2) * u) * dt
         newth = th + newthdot * dt
@@ -52,16 +65,37 @@ class AngledPendulumEnv(PendulumEnv):
         """Get reward model."""
         return PendRewardModel(self.angle)
 
+    def reset(self):
+        if self.radomize_start:
+            high = np.array([np.pi, 1])
+            self.state = self.np_random.uniform(low=-high, high=high)
+        else:
+            self.state = np.array([np.pi, 0])
+        self.last_u = None
+        return self._get_obs()
+
+    def _get_obs(self):
+        theta, thetadot = self.state
+        if self.goal_in_state:
+            return np.array([np.cos(theta), np.sin(theta), thetadot, np.cos(self.angle), np.sin(self.angle)])
+        else:
+            return np.array([np.cos(theta), np.sin(theta), thetadot])
+
 
 class MBPendulumAngleContinuousWrapper(EnvironmentWrapper):
-    def __init__(self):
+    def __init__(self, randomize_start=False, goal_in_state=False):
         super().__init__()
         self.name = "MBPendulum-v2"
         self.parameters = {
             "goal_angle": ContinuousParameter(0.0, 2 * np.pi),
         }
 
+        self.randomize_start = randomize_start
+        self.goal_in_state = goal_in_state
+
     def create_env(self, parameter_values: Dict[str, Any]):
-        environment = AngledPendulumEnv(angle=parameter_values["goal_angle"])
+        environment = AngledPendulumEnv(angle=parameter_values["goal_angle"],
+                                        randomize_start=self.randomize_start,
+                                        goal_in_state=self.goal_in_state)
         environment.reset()
         return environment
